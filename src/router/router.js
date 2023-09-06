67bab19b6552dec4a3e83b4d3dd04468a87e2d5f
masvidal778 <nuria.masvidal@gmail.com>
2023-09-06 02:33:48 +0200

    creating page and path to 404 error

--- src/router/router.js
+++ src/router/router.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import AboutPage from "@/modules/pokemon/pages/AboutPage.vue";
 import ListPage from "@/modules/pokemon/pages/ListPage.vue";
 import PokemonPage from "@/modules/pokemon/pages/PokemonPage.vue";
+import NoPageFound from "@/modules/shared/pages/NoPageFound.vue";
 
 
 const routes = [
     { path: '/', component: ListPage },
     { path: '/about', component: AboutPage },
     { path: '/id', component: PokemonPage },
+    { path: '/:pathMatch(.*)*', component: NoPageFound },
 ]
 
 const router = createRouter({
