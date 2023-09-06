2369ef7eead0b244f2626f99d50133a5f52033e5
masvidal778 <nuria.masvidal@gmail.com>
2023-09-06 17:13:36 +0200

    creating Navbar

---
+++ src/modules/shared/components/Navbar.vue
@@ -0,0 +1,19 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "Navbar"
+})
+</script>
+
+<template>
+<div>
+  <a href="/">Pokemon List</a>
+  <a href="/id">Pokemon per ID</a>
+  <a href="/about">About</a>
+</div>
+</template>
+
+<style scoped>
+
+</style>
