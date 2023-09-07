8356f3608f754117d26bba4c2f92e289b9d84f32
masvidal778 <nuria.masvidal@gmail.com>
2023-09-07 02:45:30 +0200

    Working with url segments and query parameters

--- src/modules/pokemon/pages/PokemonPage.vue
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -2,12 +2,29 @@
 import {defineComponent} from 'vue'
 
 export default defineComponent({
-  name: "PokemonPage"
+  name: "PokemonPage",
+  props: {
+    id: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      //id: null
+    }
+  },
+  created() {
+    const { id } = this.$route.params
+
+    console.log(id)
+    //this.id = id
+  }
 })
 </script>
 
 <template>
-<h1>Pokemon Page</h1>
+<h1>Pokemon Page <span>#{{ id }}</span> </h1>
 </template>
 
 <style scoped>
