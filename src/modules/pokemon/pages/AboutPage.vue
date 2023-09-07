1e4c59f245e121cf5e1c7286666996d7864db9e4
masvidal778 <nuria.masvidal@gmail.com>
2023-09-07 02:08:08 +0200

    learning about lifecycles

--- src/modules/pokemon/pages/AboutPage.vue
+++ src/modules/pokemon/pages/AboutPage.vue
@@ -2,12 +2,69 @@
 import {defineComponent} from 'vue'
 
 export default defineComponent({
-  name: "AboutPage"
+  name: "AboutPage",
+  data(){
+    return{
+      name: 'Núria'
+    }
+  },
+  methods: {
+    onChangeName(){
+      this.name = 'Judit'
+    }
+  },
+  beforeCreate() {
+    console.log('beforeCreate')
+  },
+  created() {
+    console.log('created')
+    //Peticions HTTP
+  },
+  beforeMount() {
+    console.log('beforeMount')
+  },
+  mounted() {
+    console.log('mounted')
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate')
+  },
+  updated() {
+    console.log('updated')
+  },
+  activated() {
+    console.log('activated')
+  },
+  deactivated() {
+    console.log('deactivated')
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount')
+  },
+  unmounted() {
+    console.log('unmounted')
+    //per fer alguna neteja en el component
+  },
+  errorCaptured(){
+    console.log('errorCaptured')
+  },
+  renderTracked(){
+    console.log('renderTracked')
+  },
+  renderTriggered(){
+    console.log('renderTriggered')
+  }
+
 })
 </script>
 
 <template>
   <h1>About Page</h1>
+  <hr>
+  <h2>{{ name }}</h2>
+  <button @click="onChangeName">
+    Canviar nom
+  </button>
 </template>
 
 <style scoped>
